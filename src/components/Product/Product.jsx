9f804654f9154eb2f2a48d0ada01859c622a6ac0
APORNA16 <rakshit.aporna@gmail.com>
2023-03-30 01:23:52 +0600

    add new files

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Product.css';
+
+const Product = () =>{
+    return (
+        <div>
+            <h3>This is Product</h3>
+        </div>
+    );
+};
+
+export default Product;
+    
